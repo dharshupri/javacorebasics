7f242e5bcb3f68ffeb9b0fc0ae858c0b138644e8
dharshupri <dharshini0023@gmail.com>
2018-05-30 16:00:08 +0530

    count operation

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+public static void main(String[] args) {
+	String a =" 4 ";
+	String b = "5" ;
+	System.out.println(a+b);
+	
+}
+}
---
+++ src/com/basic/countprg.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class countprg {
+	public static void main(String[]args)
+	{
+		int count=30;
+		if(count>40)
+		{
+			System.out.println(40);
+		}
+	
+			else if(count == 30)
+			{
+			System.out.println(30);
+			}
+			else
+			{
+				System.out.println("");
+			}
+}
+			
+	}
+
+
