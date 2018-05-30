86bf3df04badeb1098459f19540f0532f3e4ee1e
dharshupri <dharshini0023@gmail.com>
2018-05-30 12:49:49 +0530

    true or false

---
+++ src/com/basic/mystery2.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class mystery2
+{
+public static void main( String[] args )
+{
+int count = 1;
+
+while ( count <= 10 )
+{
+ System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+ ++count;
+ } 
+ } 
+ } 
+
+
+
+
+		 
+
