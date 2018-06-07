d5928eb970b0c99cb27cdb7deb2eb9fc7111f467
dharshupri <dharshini0023@gmail.com>
2018-06-07 12:40:46 +0530

    Parsing double

---
+++ src/com/basic/parse.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class parse {
+	
+	
+
+	public static void main(String[] arg) {
+		int no=Integer.parseInt(arg[0]);
+		int no1=Integer.parseInt(arg[1]);
+		int no2=no+no1;
+		System.out.println(no2);
+		
+		}}
+
